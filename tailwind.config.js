e92b25c39bdbb8291f1cca746ecc2b2ec1dfd7e1
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-05-29 08:54:33 +0300

    cleanup
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
-const colors = require('tailwindcss/colors')
+import colors from 'tailwindcss/colors'
 
-module.exports = {
-  theme: {
-    colors: {
-        ...colors,
-      transparent: 'transparent',
-      current: 'currentColor',
-      brand:rgb(176, 45, 33)
+export const theme = {
+  colors: {
+    ...colors,
+    transparent: 'transparent',
+    current: 'currentColor',
+    brand: rgb(176, 45, 33)
     //   black: colors.black,
     //   white: colors.white,
     //   gray: colors.gray,
     //   emerald: colors.emerald,
     //   indigo: colors.indigo,
     //   yellow: colors.yellow,
-    },
   },
 }
