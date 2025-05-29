a1cc6e0b9f61a20b0c07cff4a6fac4f7e981aaba
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-05-29 10:42:05 +0300

    add vite-plugin-devtools-json
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from "@react-router/dev/vite"
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite"
+import devtoolsJson from "vite-plugin-devtools-json"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), devtoolsJson()],
   resolve: {
     alias: {
-      ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
-    }
-  }
-});
+      ".prisma/client/index-browser":
+        "./node_modules/.prisma/client/index-browser.js",
+    },
+  },
+})
