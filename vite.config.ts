283c71e865f01b35c7430f08fb44d423668e71d5
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-06-08 11:06:08 +0300

    configure react-router-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
+import { reactRouterDevTools } from "react-router-devtools"
 import { defineConfig } from "vite"
 import devtoolsJson from "vite-plugin-devtools-json"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), devtoolsJson()],
+  plugins: [
+    reactRouterDevTools(),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    devtoolsJson(),
+  ],
   resolve: {
     alias: {
       ".prisma/client/index-browser":
