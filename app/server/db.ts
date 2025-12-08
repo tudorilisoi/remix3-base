345c2d867af9d216975c51c910256edaea962a21
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-12-08 12:17:41 +0200

    update prisma to v7
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- app/server/db.ts
+++ app/server/db.ts
@@ -1,7 +1,11 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
+import "dotenv/config"
+import { PrismaClient } from "../../prisma/generated/client.js"
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  return new PrismaClient({ adapter })
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
