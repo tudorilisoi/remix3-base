5a123c3ac67fee572a5773bd850cbbdf179e70d5
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-12-15 14:53:14 +0200

    WIP
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,8 +2,8 @@ import type { Route } from "./+types/home"
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "People over time" },
+    { name: "description", content: "Deep history" },
   ]
 }
 
