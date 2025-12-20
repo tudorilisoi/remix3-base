885d635fcb8f19fdb0f3f98f7d21cfc2f5da5abf
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-12-20 20:06:13 +0200

    cleanup
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- app/dsl/person/PersonForm.tsx
+++ app/dsl/person/PersonForm.tsx
@@ -42,67 +42,64 @@ const PersonForm: React.FC = (props) => {
     },
   })
   return (
-    <div>
-      <h1>Simple Form Example</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-          e.stopPropagation()
-          // console.log(form.)
-          form.handleSubmit()
-        }}
-      >
-        <div>
-          <form.Field
-            name="firstName"
-            children={(field) => {
-              const isInvalid =
-                field.state.meta.isTouched && !field.state.meta.isValid
-              console.log(`🚀 ~ PersonForm ~ field:`, field, isInvalid)
-              return (
-                <Field data-invalid={isInvalid}>
-                  <FieldLabel htmlFor={field.name}>First name</FieldLabel>
-                  <Input
-                    id={field.name}
-                    name={field.name}
-                    value={field.state.value}
-                    onBlur={field.handleBlur}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                    aria-invalid={isInvalid}
-                    placeholder="First (given) name"
-                    autoComplete="off"
-                  />
-                  {isInvalid && (
-                    <FieldError errors={field.state.meta.errors} />
-                    // <FieldInfo field={field} />
-                  )}
-                </Field>
-              )
-            }}
-          />
-        </div>
-        <form.Subscribe
-          selector={(state) => [state.canSubmit, state.isSubmitting]}
-          children={([canSubmit, isSubmitting]) => (
-            <>
-              <Button type="submit" disabled={!canSubmit}>
-                {isSubmitting ? "..." : "Submit"}
-              </Button>
-              <Button
-                type="reset"
-                onClick={(e) => {
-                  // Avoid unexpected resets of form elements (especially <select> elements)
-                  e.preventDefault()
-                  form.reset()
-                }}
-              >
-                Reset
-              </Button>
-            </>
-          )}
+    <form
+      onSubmit={(e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        // console.log(form.)
+        form.handleSubmit()
+      }}
+    >
+      <div>
+        <form.Field
+          name="firstName"
+          children={(field) => {
+            const isInvalid =
+              field.state.meta.isTouched && !field.state.meta.isValid
+            // console.log(`🚀 ~ PersonForm ~ field:`, field, isInvalid)
+            return (
+              <Field data-invalid={isInvalid}>
+                <FieldLabel htmlFor={field.name}>First name</FieldLabel>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  aria-invalid={isInvalid}
+                  placeholder="First (given) name"
+                  autoComplete="off"
+                />
+                {isInvalid && (
+                  <FieldError errors={field.state.meta.errors} />
+                  // <FieldInfo field={field} />
+                )}
+              </Field>
+            )
+          }}
         />
-      </form>
-    </div>
+      </div>
+      <form.Subscribe
+        selector={(state) => [state.canSubmit, state.isSubmitting]}
+        children={([canSubmit, isSubmitting]) => (
+          <>
+            <Button type="submit" disabled={!canSubmit}>
+              {isSubmitting ? "..." : "Submit"}
+            </Button>
+            <Button
+              type="reset"
+              onClick={(e) => {
+                // Avoid unexpected resets of form elements (especially <select> elements)
+                e.preventDefault()
+                form.reset()
+              }}
+            >
+              Reset
+            </Button>
+          </>
+        )}
+      />
+    </form>
   )
 }
 export default PersonForm
