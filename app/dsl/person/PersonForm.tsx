9cc1cc450fc98bedfcfccba93b11f1b99c655652
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-12-20 19:41:36 +0200

    add schema to person form
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- app/dsl/person/PersonForm.tsx
+++ app/dsl/person/PersonForm.tsx
@@ -1,8 +1,127 @@
-const PersonForm: React.FC = (props) => {
+import {
+  revalidateLogic,
+  useForm,
+  type AnyFieldApi,
+} from "@tanstack/react-form"
+import { z } from "zod"
+function FieldInfo({ field }: { field: AnyFieldApi }) {
   return (
     <>
-      <h1>Person form</h1>
+      {field.state.meta.isTouched && !field.state.meta.isValid ? (
+        <em>{field.state.meta.errors.join(",")}</em>
+      ) : null}
+      {field.state.meta.isValidating ? "Validating..." : null}
     </>
+  )
+}
+
+const schema = z.object({
+  firstName: z.string().min(1, "A first name is required"),
+  lastName: z.string().min(1, "A last name is required"),
+})
+
+const PersonForm: React.FC = (props) => {
+  const form = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+    },
+    validationLogic: revalidateLogic(),
+    validators: {
+      onDynamic: schema,
+    },
+    onSubmit: async ({ value }) => {
+      // Do something with form data
+      console.log(value)
+    },
+  })
+  return (
+    <div>
+      <h1>Simple Form Example</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          e.stopPropagation()
+          form.handleSubmit()
+        }}
+      >
+        <div>
+          {/* A type-safe field component*/}
+          <form.Field
+            name="firstName"
+            validators={{
+              onChange: ({ value }) =>
+                !value
+                  ? "A first name is required"
+                  : value.length < 3
+                    ? "First name must be at least 3 characters"
+                    : undefined,
+              onChangeAsyncDebounceMs: 500,
+              onChangeAsync: async ({ value }) => {
+                await new Promise((resolve) => setTimeout(resolve, 1000))
+                return (
+                  value.includes("error") && 'No "error" allowed in first name'
+                )
+              },
+            }}
+            children={(field) => {
+              // Avoid hasty abstractions. Render props are great!
+              return (
+                <>
+                  <label htmlFor={field.name}>First Name:</label>
+                  <input
+                    id={field.name}
+                    name={field.name}
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                  <FieldInfo field={field} />
+                </>
+              )
+            }}
+          />
+        </div>
+        <div>
+          <form.Field
+            name="lastName"
+            children={(field) => (
+              <>
+                <label htmlFor={field.name}>Last Name:</label>
+                <input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                <FieldInfo field={field} />
+              </>
+            )}
+          />
+        </div>
+        <form.Subscribe
+          selector={(state) => [state.canSubmit, state.isSubmitting]}
+          children={([canSubmit, isSubmitting]) => (
+            <>
+              <button type="submit" disabled={!canSubmit}>
+                {isSubmitting ? "..." : "Submit"}
+              </button>
+              <button
+                type="reset"
+                onClick={(e) => {
+                  // Avoid unexpected resets of form elements (especially <select> elements)
+                  e.preventDefault()
+                  form.reset()
+                }}
+              >
+                Reset
+              </button>
+            </>
+          )}
+        />
+      </form>
+    </div>
   )
 }
 export default PersonForm
